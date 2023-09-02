331334aca33287366feb91f8ccc11a072f338bc6
Akash Das <akashdas.dev@gmail.com>
2023-09-02 20:31:31 +0600

    first

---
+++ app/src/main/java/com/recogniseerror/currencyconvertermvvm/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.recogniseerror.currencyconvertermvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.recogniseerror.currencyconvertermvvm.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
